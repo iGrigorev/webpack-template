245ab9e6b066c9ae67d5187260b6177be2204f6b
iGrigorev <im.grigorev.vitaly@gmail.com>
2019-09-16 23:39:09 +0300

    p4

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,21 +1,38 @@
 const path = require('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+}
 
 module.exports = {
+  externals: {
+    paths: PATHS
+  },
   entry: {
-    app: './src/index.js'
+    app: PATHS.src
   },
 
  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist'
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: '/'
   }, 
     module: {
             rules: [{
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: '/node_modules/'
+            }, {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                }
             }, {
                 test: /\.scss$/,
                 use: [
@@ -26,7 +43,7 @@ module.exports = {
                         options: { sourceMap: true }
                     }, {
                         loader: 'postcss-loader',
-                        options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }     
+                        options: { sourceMap: true }     
                     }, {
                         loader: 'sass-loader',
                         options: { sourceMap: true }
@@ -42,7 +59,7 @@ module.exports = {
                       options: { sourceMap: true }
                     }, {
                       loader: 'postcss-loader',
-                      options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+                      options: { sourceMap: true }
                     }
                 ]
             }]
@@ -52,7 +69,16 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-          filename: "[name].css",
+          filename: `${PATHS.assets}css/[name].[hash].css`,
+        }),
+        new CopyWebpackPlugin([
+          { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+          { from: `${PATHS.src}/static`, to: '' },
+        ]),
+        new HtmlWebpackPlugin({
+          hash: false,
+          template: `${PATHS.src}/index.html`,
+          filename: 'index.html',
         })
     ]
 }
